71aeedd69d4ed2354a8f3f1e167ce0f7c7ba83dd
alvaro.freitas <alvaro.freitas@192.168.25.5>
2016-03-03 20:44:46 -0300

    Novelty

--- src/main/java/creative/framework/parser/ApparelParser.java
+++ src/main/java/creative/framework/parser/ApparelParser.java
@@ -7,111 +7,118 @@ import creative.framework.model.Apparel;
 
 public class ApparelParser implements Parser<Apparel> {
 
-	final String COLOR_WHITE = "white";
-	final String COLOR_BLACK = "black";
-	final String COLOR_NAVY = "navy";
-	final String COLOR_GRAY = "gray";
-	final String COLOR_BLUE = "blue";
-	final String COLOR_BROWN = "brown";
-	final String COLOR_LILAC = "lilac";
+    final String SHIRT_WHITE = "shirt:white";
+    final String SHIRT_NAVY  = "shirt:navy";
+    final String SHIRT_GRAY  = "shirt:gray";
+    final String SHIRT_BLUE  = "shirt:blue";
+    final String SHIRT_LILAC = "shirt:lilac";
+    final String PANTS_WHITE = "pants:white";
+    final String PANTS_BLACK = "pants:black";
+    final String PANTS_NAVY  = "pants:navy";
+    final String PANTS_GRAY  = "pants:gray";
+    final String PANTS_BROWN = "pant:brown";
+    final String SHOES_WHITE = "shoes:white";
+    final String SHOES_BLACK = "shoes:black";
+    final String SHOES_NAVY  = "shoes:navy";
+    final String SHOES_GRAY  = "shoes:gray";
+    final String SHOES_BROWN = "shoes:brown";
 
-	HashMap<String, Integer> attributes;
+    final String COLOR_WHITE = "white";
+    final String COLOR_BLACK = "black";
+    final String COLOR_NAVY  = "navy";
+    final String COLOR_GRAY  = "gray";
+    final String COLOR_BLUE  = "blue";
+    final String COLOR_BROWN = "brown";
+    final String COLOR_LILAC = "lilac";
 
-	public ApparelParser() {
+    HashMap<String, Integer> attributes;
 
-		int index = 0;
-		attributes = new HashMap<>();
+    public ApparelParser() {
 
-		attributes.put(COLOR_WHITE, index++);
-		attributes.put(COLOR_BLACK, index++);
-		attributes.put(COLOR_NAVY, index++);
-		attributes.put(COLOR_GRAY, index++);
-		attributes.put(COLOR_BLUE, index++);
-		attributes.put(COLOR_BROWN, index++);
-		attributes.put(COLOR_LILAC, index);
-	}
+        int index = 0;
+        attributes = new HashMap<>();
 
-	@Override
-	public Instance getInstance(Apparel apparel) {
+        attributes.put(SHIRT_WHITE, index++);
+        attributes.put(SHIRT_NAVY, index++);
+        attributes.put(SHIRT_GRAY, index++);
+        attributes.put(SHIRT_BLUE, index++);
+        attributes.put(SHIRT_LILAC, index++);
+        attributes.put(PANTS_WHITE, index++);
+        attributes.put(PANTS_BLACK, index++);
+        attributes.put(PANTS_NAVY, index++);
+        attributes.put(PANTS_GRAY, index++);
+        attributes.put(PANTS_BROWN, index++);
+        attributes.put(SHOES_WHITE, index++);
+        attributes.put(SHOES_BLACK, index++);
+        attributes.put(SHOES_NAVY, index++);
+        attributes.put(SHOES_GRAY, index++);
+        attributes.put(SHOES_BROWN, index++);
 
-		double[] dataInstance = new double[attributes.size()];
+    }
 
-		switch (apparel.getPants()) {
+    @Override
+    public Instance getInstance(Apparel apparel) {
 
-		case COLOR_WHITE:
-			dataInstance[attributes.get(COLOR_WHITE)]++;
-			break;
-		case COLOR_BLACK:
-			dataInstance[attributes.get(COLOR_BLACK)]++;
-			break;
-		case COLOR_NAVY:
-			dataInstance[attributes.get(COLOR_NAVY)]++;
-			break;
-		case COLOR_GRAY:
-			dataInstance[attributes.get(COLOR_GRAY)]++;
-			break;
-		case COLOR_BLUE:
-			dataInstance[attributes.get(COLOR_BLUE)]++;
-			break;
-		case COLOR_BROWN:
-			dataInstance[attributes.get(COLOR_BROWN)]++;
-			break;
-		case COLOR_LILAC:
-			dataInstance[attributes.get(COLOR_LILAC)]++;
-			break;
-		}
+        double[] dataInstance = new double[attributes.size()];
 
-		switch (apparel.getShirt()) {
+        switch (apparel.getPants()) {
 
-		case COLOR_WHITE:
-			dataInstance[attributes.get(COLOR_WHITE)]++;
-			break;
-		case COLOR_BLACK:
-			dataInstance[attributes.get(COLOR_BLACK)]++;
-			break;
-		case COLOR_NAVY:
-			dataInstance[attributes.get(COLOR_NAVY)]++;
-			break;
-		case COLOR_GRAY:
-			dataInstance[attributes.get(COLOR_GRAY)]++;
-			break;
-		case COLOR_BLUE:
-			dataInstance[attributes.get(COLOR_BLUE)]++;
-			break;
-		case COLOR_BROWN:
-			dataInstance[attributes.get(COLOR_BROWN)]++;
-			break;
-		case COLOR_LILAC:
-			dataInstance[attributes.get(COLOR_LILAC)]++;
-			break;
-		}
+            case COLOR_WHITE:
+                dataInstance[attributes.get(PANTS_WHITE)]++;
+                break;
+            case COLOR_BLACK:
+                dataInstance[attributes.get(PANTS_BLACK)]++;
+                break;
+            case COLOR_NAVY:
+                dataInstance[attributes.get(PANTS_NAVY)]++;
+                break;
+            case COLOR_GRAY:
+                dataInstance[attributes.get(PANTS_GRAY)]++;
+                break;
+            case COLOR_BROWN:
+                dataInstance[attributes.get(PANTS_BROWN)]++;
+                break;
+        }
 
-		switch (apparel.getShoes()) {
+        switch (apparel.getShirt()) {
 
-		case COLOR_WHITE:
-			dataInstance[attributes.get(COLOR_WHITE)]++;
-			break;
-		case COLOR_BLACK:
-			dataInstance[attributes.get(COLOR_BLACK)]++;
-			break;
-		case COLOR_NAVY:
-			dataInstance[attributes.get(COLOR_NAVY)]++;
-			break;
-		case COLOR_GRAY:
-			dataInstance[attributes.get(COLOR_GRAY)]++;
-			break;
-		case COLOR_BLUE:
-			dataInstance[attributes.get(COLOR_BLUE)]++;
-			break;
-		case COLOR_BROWN:
-			dataInstance[attributes.get(COLOR_BROWN)]++;
-			break;
-		case COLOR_LILAC:
-			dataInstance[attributes.get(COLOR_LILAC)]++;
-			break;
-		}
+            case COLOR_WHITE:
+                dataInstance[attributes.get(SHIRT_WHITE)]++;
+                break;
+            case COLOR_NAVY:
+                dataInstance[attributes.get(SHIRT_NAVY)]++;
+                break;
+            case COLOR_GRAY:
+                dataInstance[attributes.get(SHIRT_GRAY)]++;
+                break;
+            case COLOR_BLUE:
+                dataInstance[attributes.get(SHIRT_BLUE)]++;
+                break;
+            case COLOR_LILAC:
+                dataInstance[attributes.get(SHIRT_LILAC)]++;
+                break;
+        }
 
-		return new Instance(dataInstance);
-	}
+        switch (apparel.getShoes()) {
+
+            case COLOR_WHITE:
+                dataInstance[attributes.get(SHOES_WHITE)]++;
+                break;
+            case COLOR_BLACK:
+                dataInstance[attributes.get(SHOES_BLACK)]++;
+                break;
+            case COLOR_NAVY:
+                dataInstance[attributes.get(SHOES_NAVY)]++;
+                break;
+            case COLOR_GRAY:
+                dataInstance[attributes.get(SHOES_GRAY)]++;
+                break;
+            case COLOR_BROWN:
+                dataInstance[attributes.get(SHOES_BROWN)]++;
+                break;
+        }
+
+        return new Instance(dataInstance);
+    }
 
 }
